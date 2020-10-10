553eda4a9b0a9cb762ab05624d0adf7c9a94dbad
npseyeta <eyetanachpaksorn@gmail.com>
2020-10-10 22:23:06 +0700

    Update DataSource

--- src/main/java/th/ac/kmitl/atm/DataSourceDB.java
+++ src/main/java/th/ac/kmitl/atm/DataSourceDB.java
@@ -1,7 +1,5 @@
 package th.ac.kmitl.atm;
 
-import org.springframework.stereotype.Component;
-
 import java.util.HashMap;
 import java.util.Map;
 
--- src/main/java/th/ac/kmitl/atm/DataSourceFile.java
+++ src/main/java/th/ac/kmitl/atm/DataSourceFile.java
@@ -1,8 +1,6 @@
 package th.ac.kmitl.atm;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import java.io.FileNotFoundException;
